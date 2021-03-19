5eb6c534537be09756df1733da7bff70f5987ae2
ZakobG <59724611+ZakobG@users.noreply.github.com>
2021-03-19 11:03:49 +0100

    Hope it works!

--- src/main/java/zoo/managers/ZooManagers.java
+++ src/main/java/zoo/managers/ZooManagers.java
@@ -2,12 +2,40 @@ package zoo.managers;
 
 import zoo.entities.ZooEntities;
 
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 
 public class ZooManagers {
     private ArrayList<ZooEntities> zooEntities = new ArrayList<>();
 
-    public ArrayList<ZooEntities> getAll(){
+    private int id = 0;
+    static String name;
+    static String type;
+    static int age;
+
+    public ArrayList<ZooEntities> getAll() {
         return zooEntities;
+    }
+
+    public void get() {
+        if (zooEntities.get(id) != null) {
+            Response.ok(zooEntities.get(id)).build();
+        } else {
+            Response.ok("Zvíře není.").build();
+        }
+    }
+
+    public void create(ZooEntities post) {
+        if (name != null && type != null && zooEntities.get(age) != null) {
+            post.setId(id);
+            id++;
+            zooEntities.add(post);
+        }
+    }
+
+    public void delete() {
+        if (zooEntities.get(id) != null) {
+            zooEntities.remove(id);
+        }
     }
 }
